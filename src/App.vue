53e83d2f0406fa147731f0b03f80ce945198305e
刘朝泽 <liuchaoze@itcast.cn>
2019-07-27 18:47:35 +0800

    rem适配

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,19 @@
 
     <van-button type="primary">主要按钮</van-button>
 
+    <div class="test"></div>
+
     <router-view/>
   </div>
 </template>
 
 <style lang="less" scoped>
+// 需求
+// 自己的设计稿的参照尺寸 750
+// div满屏宽->750px
+.test {
+  width: 700px;
+  height: 200px;
+  background-color: yellow;
+}
 </style>
